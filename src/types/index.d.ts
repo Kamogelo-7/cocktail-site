78d6c26cfa78917b5f180af4742d750581b568b5
Kamogelo <ekmogasoa@gmail.com>
2025-09-01 17:25:30 +0200

    added global types

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+interface Cocktail {
+  name: string;
+  country: string;
+  detail: string;
+  price: string;
+}
+
+interface Mocktail {
+  name: string;
+  country: string;
+  detail: string;
+  price: string;
+}
