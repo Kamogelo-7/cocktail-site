ffe037ef0653a64740cc4587872e034f5c4e3ba8
Kamogelo <ekmogasoa@gmail.com>
2025-09-02 11:24:27 +0200

    Added about page component

--- src/features/home/homepage.tsx
+++ src/features/home/homepage.tsx
@@ -1,9 +1,13 @@
+import AboutPage from "../about/aboutPage";
+import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
+      <Cocktails />
+      <AboutPage />
     </main>
   );
 };
